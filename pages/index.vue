2366425207d7889757e22142930ab4ad0ad93cbb
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2023-05-31 15:09:30 -0300

    feat: definied default views to login and index [logged] page

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="card border-0 p-5 shadow">
+    <span class="text-info fs-4">Olá fulano de tal, você está logado.</span>
+    <button class="btn btn-outline-danger mt-4" type="button">Sair</button>
+  </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import CellPhoneForm from '~/components/CellPhoneForm.vue';
+  import PinVerification from '~/components/PinVerification.vue';
+  
+  export default defineComponent({
+    name: 'LoginPage',
+    components: {
+      CellPhoneForm,
+      PinVerification
+    },
+
+    data(){
+      return{
+        currentComponent: 'CellPhoneForm',
+        componentKey: 0
+      }
+    },
+
+    methods: {
+      switchComponent(){
+        this.componentKey += 1
+        if(this.currentComponent === 'CellPhoneForm'){
+          this.currentComponent = 'PinVerification'
+        }else{
+          this.currentComponent = 'CellPhoneForm'
+        }
+      }
+    }
+  })
+</script>
+<template>
+  <transition name="page" mode="out-in">
+    <component :is="currentComponent" key="componentKey" @switchComponent="switchComponent"></component>
+  </transition>
+</template>
+<style scoped>
+  /* transitions.css */
+  .page-enter-active,
+  .page-leave-active {
+    transition: opacity 0.5s;
+  }
+
+  .page-enter,
+  .page-leave-to {
+    opacity: 0;
+  }
+
+</style>
